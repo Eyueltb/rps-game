7480e278e23f9a70d65fe8d57fc392874675f524
Eyuel <eyueltb@gmail.com>
2021-10-23 17:48:08 +0200

    getTokenById(@PathVariable String id), verifyToken(@PathVariable String token) is created

--- src/main/java/com/rps/controllers/TokenController.java
+++ src/main/java/com/rps/controllers/TokenController.java
@@ -2,7 +2,6 @@ package com.rps.controllers;
 
 import com.rps.dto.TokenDTO;
 import com.rps.exceptions.UseException;
-import com.rps.exceptions.UseExceptionType;
 import com.rps.models.CreateName;
 import com.rps.models.Token;
 import com.rps.services.TokenService;
@@ -10,6 +9,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.rps.exceptions.UseExceptionType.TOKEN_NOT_FOUND;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,17 +25,25 @@ public class TokenController {
     public String createToken() {
         return tokenService.createToken().getId();
     }
+    @GetMapping("/{id}")
+    public TokenDTO getTokenById(@PathVariable String id) throws UseException {
+        return tokenService.getTokenById(id).map(this::toTokenDTO).orElseThrow(()->new UseException(TOKEN_NOT_FOUND));
+    }
+    @GetMapping("/verify/{token}")
+    public boolean verifyToken(@PathVariable String token) {
+        return tokenService.verifyToken(token);
+    }
     @PostMapping("/name")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Token> setName(@RequestBody CreateName createName,
                                          @RequestHeader (value="token", required = false) String tokenId) throws UseException {
-        Token token=tokenService.getTokenById(tokenId).orElseThrow(()->new UseException(UseExceptionType.TOKEN_NOT_FOUND));
+        Token token=tokenService.getTokenById(tokenId).orElseThrow(()->new UseException(TOKEN_NOT_FOUND));
 
         if(token!=null)
             token.setName(createName.getName());
         tokenService.saveToken(token);
        //return new ResponseEntity(toTokenDTO(token), HttpStatus.CREATED);
-       return null;
+        return null;
     }
     private TokenDTO toTokenDTO(Token token){
         return new TokenDTO(
