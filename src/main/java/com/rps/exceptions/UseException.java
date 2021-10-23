98fbc19db3b6054179dc32ddaf1bed4d73882761
Eyuel <eyueltb@gmail.com>
2021-10-23 13:52:51 +0200

    Exception for the game is ready.

---
+++ src/main/java/com/rps/exceptions/UseException.java
@@ -0,0 +1,13 @@
+package com.rps.exceptions;
+
+public class UseException extends Exception {
+    UseExceptionType useExceptionType;
+    public UseException(UseExceptionType useExceptionType) {
+        super(" failed because "+ useExceptionType);
+        this.useExceptionType = useExceptionType;
+    }
+    public UseExceptionType getUseExceptionType() {
+        return useExceptionType;
+    }
+
+}
---
+++ src/main/java/com/rps/exceptions/UseExceptionType.java
@@ -0,0 +1,7 @@
+package com.rps.exceptions;
+
+public enum UseExceptionType {
+    TOKEN_NOT_FOUND,
+    GAME_NOT_FOUND,
+    GAME_ALREADY_STARTED
+}
