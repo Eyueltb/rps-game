35fc14ebf24e7d0fce5395e92c1a6009033c669c
Eyuel <eyueltb@gmail.com>
2021-10-23 18:15:14 +0200

    pom modified to support postgres

--- src/main/java/com/rps/repositories/TokenRepository.java
+++ src/main/java/com/rps/repositories/TokenRepository.java
@@ -2,6 +2,8 @@ package com.rps.repositories;
 
 import com.rps.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, String> {
 }
