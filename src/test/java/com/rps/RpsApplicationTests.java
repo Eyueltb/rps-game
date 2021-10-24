f26fabd403319ea5f553831b54a5c9a60291bc64
Eyuel <eyueltb@gmail.com>
2021-10-24 16:22:20 +0200

    Test is started!!

--- src/test/java/com/rps/RpsApplicationTests.java
+++ src/test/java/com/rps/RpsApplicationTests.java
@@ -1,13 +1,35 @@
 package com.rps;
 
+import com.rps.controllers.GameController;
+import com.rps.controllers.TokenController;
+import com.rps.dto.GameDTO;
+import com.rps.dto.TokenDTO;
+import com.rps.enums.Move;
+import com.rps.enums.Status;
+import com.rps.exceptions.UseException;
+import com.rps.models.CreateName;
+import com.rps.models.Token;
+import com.rps.repositories.GameRepository;
+import com.rps.repositories.TokenRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integrationtest")
 class RpsApplicationTests {
+    @Autowired TokenController tokenController;
+    @Autowired TokenRepository tokenRepository;
+    @Autowired GameController gameController;
+    @Autowired GameRepository gameRepository;
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
